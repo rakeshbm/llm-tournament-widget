fb23e27886cc2a8cd19c4361552707fca594e78c
rakeshbm <rakeshbmali94@gmail.com>
2025-07-26 11:07:31 -0700

    add Flask extensions, config, and requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///llm-tournament.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
